d38cac9bf01092bcfb26209ff1c6c50ec1cbce0c
Osa Mokharji <mokharjiosa@yahoo.com>
2020-02-02 12:23:11 +0530

    Some major changes

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ from models import *
 import sqlalchemy
 cogcli='7mbneubah8favrjhefcn79taum'
 cog = boto3.client('cognito-idp', region_name='ap-south-1')
+iotcore=boto3.client('iot-data')
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ITdept(4895@rds.c2ocfdyvtbwu.ap-south-1.rds.amazonaws.com:5432/postgres'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,9 +22,8 @@ def hello():
 @application.route('/test',methods = ['GET','POST'])   
 def test():
     content = json.loads(request.data)
-    # print(Locks.query.get(content['lockid']).username)
     try:
-        print(Acl.query.filter_by(lockid = content['lockid']).filter_by(username = content['username']).one())
+        print(Acl.query.filter_by(lockId = content['lockId']).filter_by(username = content['username']).one())
         return 'true'
     except NoResultFound:
         return 'false'
@@ -32,23 +32,50 @@ def test():
 def addLock():
     try:
         content = json.loads(request.data)
-        lock = Locks(content['lockid'], content['username'])
+        lock = Locks(content['lockId'], content['username'])
         db.session.add(lock)
         db.session.commit()
         return 'true'
+    except sqlalchemy.orm.exc.NoResultFound:
+        return 'false'
+    except sqlalchemy.exc.IntegrityError as e:
+        return 'duplicate'
     except Exception as e:
-        return str(e)  
+        return str(e) 
 
+def addLog(content):
+    try:
+        lg = Logs(content['lockId'], content['username'], content['operation'], content['userType'])
+        db.session.add(lg)
+        db.session.commit()
+        return 'true'
+    except Exception as e:
+        return str(e)
+
+@application.route('/changePassword', methods = ['GET', 'POST'])
+def changePassword():
+    try:
+        content=json.loads(request.data)
+        cog.change_password( PreviousPassword=content['previousPassword'],ProposedPassword=content['newPassword'],AccessToken=content['accessToken'])
+        return 'true'
+    except cog.exceptions.UserNotFoundException:
+        return 'User does not exist'
+    except cog.exceptions.InvalidParameterException:
+        return 'Email not verified'
+    except cog.exceptions.InvalidPasswordException:
+        return 'Invalid Password'
+    except Exception as e:
+        return str(e)
 
 @application.route('/checkPermission',methods = ['GET','POST'])
 def checkPermission():
     try:
         content = json.loads(request.data)
-        if Locks.query.get(content['lockid']).username == content['username']:
-            content['user_type'] = 'owner'
+        if Locks.query.get(content['lockId']).username == content['username']:
+            content['userType'] = 'owner'
         else:
-            ac = Acl.query.filter_by(lockid = content['lockid']).filter_by(username = content['username']).one()
-            content['user_type'] = 'guest'
+            ac = Acl.query.filter_by(lockId = content['lockId']).filter_by(username = content['username']).one()
+            content['userType'] = 'guest'
         content['operation'] = 'lock'
         addLog(content)
         return 'true'
@@ -57,13 +84,31 @@ def checkPermission():
     except Exception as e:
         return str(e)
 
+@application.route('/confirmForgotPassword', methods= ['GET', 'POST'])
+def confirmForgotPassword():
+    try:
+        content=json.loads(request.data)
+        cog.confirm_forgot_password(ClientId=cogcli, Username=content['username'], ConfirmationCode=content['code'], Password=content['password'])
+        return 'true'
+    except cog.exceptions.UserNotFoundException:
+        return 'User does not exist. Try Again!' 
+    except cog.exceptions.InvalidParameterException:
+        return 'Email not verified'
+    except cog.exceptions.CodeMismatchException:
+        return 'Invalid Confirmation Code'
+    except Exception as e:
+        return str(e)
+
 @application.route('/deleteLock',methods = ['GET','POST'])
 def deleteLock():
     try:
         content = json.loads(request.data)
-        Locks.query.get(content['lockid']).delete()
+        lock = Locks.query.get(content['lockId'])
+        db.session.delete(lock)
         db.session.commit()
         return 'true'
+    except sqlalchemy.orm.exc.NoResultFound:
+        return 'false'
     except Exception as e:
         return str(e)
 
@@ -71,29 +116,9 @@ def deleteLock():
 def editLock():
     try:
         content = json.loads(request.data)
-        lock = Locks.query.get(content['lockid'])
+        lock = Locks.query.get(content['lockId'])
         lock.address = content['address']
         lock.alias = content['alias']
-        db.session.commit()
-        return 'true'
-    except Exception as e:
-        return str(e)
-
-@application.route('/grantPermission', methods = ['GET','POST'])
-def grantPermission():
-    try:
-        content = json.loads(request.data)
-        rec = Acl(content['lockid'], content['username'], content['expiry'])
-        db.session.add(rec)
-        db.session.commit()
-        return 'true'
-    except Exception as e:
-        return str(e)
-
-def addLog(content):
-    try:
-        lg = Logs(content['lockid'], content['username'], content['operation'], content['user_type'])
-        db.session.add(lg)
         db.session.commit()
         return 'true'
     except Exception as e:
@@ -108,14 +133,16 @@ def getLocks():
         print(lcks)
         for lock in lcks:
             dct = {}
-            dct['lockId'] = lock.lockid
+            dct['lockId'] = lock.lockId
             dct['alias'] = lock.alias
             dct['address'] = lock.address
             dct['favourite'] = lock.favourite
-            lockDict[lock.lockid] = dct
+            lockDict[lock.lockId] = dct
         return lockDict
+    except sqlalchemy.orm.exc.NoResultFound:
+        return 'false'
     except Exception as e:
-        return str(e)
+        return str(e) 
 
 @application.route('/getOtherLocks', methods = ['GET','POST'])
 def getOtherLocks():
@@ -126,13 +153,95 @@ def getOtherLocks():
         print(lcks)
         for lock in lcks:
             dct = {}
-            dct['lockId'] = lock.lockid
+            dct['lockId'] = lock.lockId
             dct['expiry'] = lock.expiry
-            lockDetails = Locks.query.get(lock.lockid)
+            lockDetails = Locks.query.get(lock.lockId)
             dct['alias'] = lockDetails.alias
             dct['address'] = lockDetails.address
-            lockDict[lock.lockid] = dct
+            lockDict[lock.lockId] = dct
         return lockDict
+    except sqlalchemy.orm.exc.NoResultFound:
+        return 'false'
+    except Exception as e:
+        return str(e)
+
+@application.route('/getLogs', methods= ['GET', 'POST'])
+def viewLogs():
+    try:
+        content = json.loads(request.data)
+        dct = []
+        if content['choice'] == 'all':
+            logs = Logs.query.filter_by(username = content['username']).order_by(Logs.time.desc()).all()
+        elif content['choice'] == 'lockId':
+            logs = Logs.query.filter_by(username = content['username']).filter_by(lockId = content['lockId']).order_by(Logs.time.desc()).all()
+        elif content['choice'] == 'operation':
+            logs = Logs.query.filter_by(username = content['username']).filter_by(operation = content['operation']).order_by(Logs.time.desc()).all()
+        elif content['choice'] == 'userType':
+            logs = Logs.query.filter_by(username = content['username']).filter_by(userType = content['userType']).order_by(Logs.time.desc()).all()    
+        elif content['choice'] == 'time':
+            logs = Logs.query.filter_by(username = content['username']).filter(time.between(datetime.strptime(content['start'],"%Y-%m-%d %H:%M:%S"), datetime.strptime(content['end'],"%Y-%m-%d %H:%M:%S"))).order_by(Logs.time.desc()).all()
+        for log in logs:
+            indict = {}
+            indict['lockId'] = log.lockId
+            indict['username'] = log.username
+            indict['time'] = datetime.strftime(log.time)
+            indict['userType'] = log.userType
+            indict['operation'] = log.operation
+            dct.append(indict)
+        return str(dct)
+    except sqlalchemy.orm.exc.NoResultFound:
+        return 'false'
+    except Exception as e:
+        return str(e)
+
+@application.route('/getUsers', methods = ['GET', 'POST'])
+def getUsers():
+    try:
+        content = json.loads(request.data)
+        q=db.session.query(Users).filter_by(lockId = content['lockId']).join(Locks).join(Acl).all()
+        dct={}
+        for row in q:
+            indict = {}
+            indcit['name'] = row.name
+            indict['userType'] = row.userType
+            indict['expiry'] = row.expiry
+            dct['username'] = row.username
+        return dct
+    except sqlalchemy.orm.exc.NoResultFound:
+        return 'false'
+    except Exception as e:
+        return str(e)
+
+@application.route('/grantPermission', methods = ['GET','POST'])
+def grantPermission():
+    try:
+        content = json.loads(request.data)
+        rec = Acl(content['lockId'], content['username'], content['expiry'])
+        db.session.add(rec)
+        db.session.commit()
+        return 'true'
+    except Exception as e:
+        return str(e)
+
+@application.route('/forgotPassword', methods= ['GET', 'POST'])
+def forgotPassword():
+    try:
+        content = json.loads(request.data)
+        cog.forgot_password(ClientId=cogcli, Username=content['username'])
+        return 'true'
+    except cog.exceptions.UserNotFoundException:
+        return 'User does not exist' 
+    except Exception as e:
+        return str(e)
+
+@application.route('/lockOperations', methods = ['GET', 'POST'])
+def lockOperations():
+    try:
+        content = json.loads(request.data)
+        pl = {'lockId' : content['lockId'], operation : content['opration']}
+        response = client.publish(topic = 'string', qos = 123, payload = pl)
+        addLog(content)
+        return str(response)
     except Exception as e:
         return str(e)
 
@@ -148,12 +257,14 @@ def login():
             },
             ClientId = cogcli
         )
+        retval = auth["AuthenticationResult"]["AccessToken"]
         resp = Users.query.get(content['username'])
-        new_arr = resp.appids.copy()
-        new_arr.append(content['appId'])
-        resp.appids = new_arr
-        db.session.commit()
-        return 'true'
+        if not content['lockId'] in resp.appIds:
+            new_arr = resp.appIds.copy()
+            new_arr.append(content['appId'])
+            resp.appIds = new_arr
+            db.session.commit()
+        return retval
     except cog.exceptions.UserNotConfirmedException:
         return 'User is not confirmed. Please check your mail.'
     except cog.exceptions.UserNotFoundException:
@@ -162,6 +273,20 @@ def login():
         return 'Username/Password is incorrect'
     except Exception as e:
         return 'Unknown error. Please contact the developer.'
+
+@application.route('/logout', methods = ['GET', 'POST'])
+def logout():
+    try:
+        content = json.loads(request.data)
+        cog.global_sign_out(AccessToken = content['AccessToken'])
+        resp = Users.query.get(content['username'])
+        new_arr = resp.appIds.copy()
+        new_arr.remove(content['appId'])
+        resp.appIds = new_arr
+        db.session.commit()
+        return 'true'
+    except Exception as e:
+        return str(e)
 
 @application.route('/signup',methods = ['GET','POST'])
 def signup():
@@ -187,16 +312,17 @@ def signup():
 def toggleFavourite():
     try:
         content = json.loads(request.data)
-        lock = Locks.query.get(content['lockid'])
+        lock = Locks.query.get(content['lockId'])
         if content['choice'] == 'fav':
             lock.favourite = 'true'
         else:
             lock.favourite = 'false'
         db.session.commit()
         return 'true'
+    except sqlalchemy.orm.exc.NoResultFound:
+        return 'false'
     except Exception as e:
         return str(e)
-
 
 if __name__ == '__main__':
     application.run(debug = True)
--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Users(db.Model):
     username = db.Column(db.Text, primary_key = True)
     name = db.Column(db.Text, nullable = False)
     phone = db.Column(db.Numeric, nullable = False)
-    appids = db.Column(postgresql.ARRAY(db.Text))
+    appIds = db.Column(postgresql.ARRAY(db.Text))
 
     acl = db.relationship('Acl')
     logs = db.relationship('Logs')
@@ -25,7 +25,7 @@ class Users(db.Model):
 class Locks(db.Model):
     __tablename__ = 'locks'
 
-    lockid = db.Column(db.Text, primary_key = True)
+    lockId = db.Column(db.Text, primary_key = True)
     alias = db.Column(db.Text)
     address = db.Column(db.Text)
     favourite = db.Column(db.Boolean, default = False)
@@ -34,19 +34,19 @@ class Locks(db.Model):
     acl = db.relationship('Acl')
     logs = db.relationship('Logs')
 
-    def __init__(self, lockid, username):
-        self.lockid = lockid
+    def __init__(self, lockId, username):
+        self.lockId = lockId
         self.username = username
 
 class Acl(db.Model):
     __tablename__ = 'acl'
 
-    lockid = db.Column(db.Text, db.ForeignKey('locks.lockid'), primary_key = True)
+    lockId = db.Column(db.Text, db.ForeignKey('locks.lockId'), primary_key = True)
     username = db.Column(db.Text, db.ForeignKey('users.username'), primary_key = True)
     expiry = db.Column(postgresql.TIMESTAMP, nullable = False)
 
-    def __init__(self, lockid, username, expiry):
-        self.lockid = lockid
+    def __init__(self, lockId, username, expiry):
+        self.lockId = lockId
         self.username = username
         self.expiry = expiry
 
@@ -54,14 +54,14 @@ class Logs(db.Model):
     __tablename__ = 'logs'
 
     serial = db.Column(db.Integer, primary_key = True)
-    lockid = db.Column(db.Text, db.ForeignKey('locks.lockid'))
+    lockId = db.Column(db.Text, db.ForeignKey('locks.lockId'))
     username = db.Column(db.Text, db.ForeignKey('users.username'))
     time = db.Column(postgresql.TIMESTAMP, default = datetime.now(timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M'))
     operation = db.Column(db.Text, nullable = False)
     user_type = db.Column(db.Text, nullable = False)
 
-    def __init__(self, lockid, username, operation, user_type):
-        self.lockid = lockid
+    def __init__(self, lockId, username, operation, user_type):
+        self.lockId = lockId
         self.username = username
         self.operation = operation
         self.user_type = user_type
